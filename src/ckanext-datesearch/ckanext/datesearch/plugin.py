c0e27966da9c8503be1fd7f6982d27ac489a8888
Varun Ratnakar <varunratnakar@gmail.com>
2023-07-12 21:20:46 +0530

    Exposing solr port & fixes for date range search

--- src/ckanext-datesearch/ckanext/datesearch/plugin.py
+++ src/ckanext-datesearch/ckanext/datesearch/plugin.py
@@ -16,26 +16,30 @@ class DateSearchPlugin(plugins.SingletonPlugin):
         toolkit.add_resource('public', 'ckanext-datesearch')
 
     def before_search(self, search_params):
+        return self.before_dataset_search(search_params)
+
+    def before_dataset_search(self, search_params):
         extras = search_params.get('extras')
         if not extras:
             # There are no extras in the search params, so do nothing.
             return search_params
 
-        start_date = extras.get('ext_startdate')
-
-        end_date = extras.get('ext_enddate')
+        start_date = extras.get('ext_start_date')
+        end_date = extras.get('ext_end_date')
 
         if not start_date and not end_date:
             # The user didn't select either a start and/or end date, so do nothing.
             return search_params
+
         if not start_date:
             start_date = '*'
+
         if not end_date:
             end_date = '*'
 
         # Add a date-range query with the selected start and/or end dates into the Solr facet queries.
         fq = search_params.get('fq', '')
-        fq = '{fq} +extras_PublicationTimestamp:[{sd} TO {ed}]'.format(fq=fq, sd=start_date, ed=end_date)
+        fq = '{fq} +extras_temporal_range:[{sd} TO {ed}]'.format(fq=fq, sd=start_date, ed=end_date)
 
         search_params['fq'] = fq
 
--- src/ckanext-datesearch/ckanext/datesearch/public/js/daterangepicker-module.js
+++ src/ckanext-datesearch/ckanext/datesearch/public/js/daterangepicker-module.js
@@ -9,37 +9,35 @@ this.ckan.module('daterangepicker-module', function ($, _) {
             };
 
             // Pick out relevant parameters
-            param_start = $.urlParam('ext_startdate');
-            param_end = $.urlParam('ext_enddate');
+            param_start = $.urlParam('ext_start_date');
+            param_end = $.urlParam('ext_end_date');
 
             // Populate the datepicker and hidden fields
             if (param_start) {
-                $('#datepicker #start').val(moment.utc(param_start).year());
-                $('#ext_startdate').val(param_start);
+                $('#datepicker #start').val(moment.utc(param_start).format("YYYY-MM-DD"));
+                $('#ext_start_date').val(param_start);
             }
             if (param_end) {
-                $('#datepicker #end').val(moment.utc(param_end).year());
-                $('#ext_enddate').val(param_end);
+                $('#datepicker #end').val(moment.utc(param_end).format("YYYY-MM-DD"));
+                $('#ext_end_date').val(param_end);
             }
 
-            // Add hidden <input> tags #ext_startdate and #ext_enddate, if they don't already exist.
+            // Add hidden <input> tags #ext_start_date and #ext_end_date, if they don't already exist.
             var form = $("#dataset-search");
             // CKAN 2.1
             if (!form.length) {
                 form = $(".search-form");
             }
-            if ($("#ext_startdate").length === 0) {
-                $('<input type="hidden" id="ext_startdate" name="ext_startdate" />').appendTo(form);
+            if ($("#ext_start_date").length === 0) {
+                $('<input type="hidden" id="ext_start_date" name="ext_start_date" />').appendTo(form);
             }
-            if ($("#ext_enddate").length === 0) {
-                $('<input type="hidden" id="ext_enddate" name="ext_enddate" />').appendTo(form);
+            if ($("#ext_end_date").length === 0) {
+                $('<input type="hidden" id="ext_end_date" name="ext_end_date" />').appendTo(form);
             }
 
             // Add a date-range picker widget to the <input> with id #daterange
             $('#datepicker.input-daterange').datepicker({
-                format: "yyyy",
-                startView: 3,
-                minViewMode: 2,
+                format: "yyyy-mm-dd",
                 keyboardNavigation: false,
                 autoclose: true
             }).on('changeDate', function (ev) {
@@ -47,23 +45,23 @@ this.ckan.module('daterangepicker-module', function ($, _) {
 
                     // Format the start and end dates into strings in a date format that Solr understands.
                     var v = moment(ev.date);
-                    var fs = 'YYYY-MM-DDTHH:mm:ss';
+                    var fs = 'YYYY-MM-DD';
 
                     switch (ev.target.name) {
                         case 'start':
-                            // Set the value of the hidden <input id="ext_startdate"> to the chosen start date.
+                            // Set the value of the hidden <input id="ext_start_date"> to the chosen start date.
                             if (ev.date) {
-                                $('#ext_startdate').val(v.format(fs) + 'Z');
+                                $('#ext_start_date').val(v.format(fs));
                             } else {
-                                $('#ext_startdate').val('');
+                                $('#ext_start_date').val('');
                             }
                             break;
                         case 'end':
-                            // Set the value of the hidden <input id="ext_enddate"> to the chosen end date.
+                            // Set the value of the hidden <input id="ext_end_date"> to the chosen end date.
                             if (ev.date) {
-                                $('#ext_enddate').val(v.add('y', 1).subtract('s', 1).format(fs) + 'Z');
+                                $('#ext_end_date').val(v.format(fs));
                             } else {
-                                $('#ext_enddate').val('');
+                                $('#ext_end_date').val('');
                             }
                             break;
                     }
